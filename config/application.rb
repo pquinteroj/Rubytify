02b3b8004623347a79d7ef3a73f2c1ea26a87918
pquinteroj <pcqj1978@gmail.com>
2019-12-22 19:53:51 -0500

    Actualizacion de Url cros

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module Rubytify
     config.api_only = true
 
     config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://localhost:3000',
+    'Access-Control-Allow-Origin' => 'https://secret-atoll-80529.herokuapp.com/',
     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
     }
 
